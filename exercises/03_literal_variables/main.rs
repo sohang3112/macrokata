6325761b7a3299a7166597d94474644da6c04e43
Sohang Chopra <sohangchopra@gmail.com>
2022-10-28 18:11:04 +0530

    03_literal_variables - DONE

--- exercises/03_literal_variables/main.rs
+++ exercises/03_literal_variables/main.rs
@@ -4,7 +4,10 @@ fn print_result(num: i32) {
 }
 ////////// DO NOT CHANGE ABOVE HERE /////////
 
-// TODO: create `math!()` macro.
+macro_rules! math {
+    ($x:literal plus $y:literal) => { $x + $y };
+    (square $x:literal) => { $x * $x };
+}
 
 ////////// DO NOT CHANGE BELOW HERE /////////
 
